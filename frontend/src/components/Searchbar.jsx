f1397a353e87d5708ece5bfec4b6b5777ebeeaf6
Sasi Vaithiyanathan <sasi.vaith@gmail.com>
2025-11-07 23:59:19 -0500

    fixed filter

--- frontend/src/components/Searchbar.jsx
+++ frontend/src/components/Searchbar.jsx
@@ -51,7 +51,9 @@ const Searchbar = ({ apiBaseUrl = DEFAULT_API_BASE_URL }) => {
     }
   }
 
-  return params.toString()
+  const queryString = params.toString()
+  console.log('Built query string:', queryString)
+  return queryString
 }
 
   const handleSearch = (event) => {
@@ -70,6 +72,7 @@ const Searchbar = ({ apiBaseUrl = DEFAULT_API_BASE_URL }) => {
 
     setErrorMessage('') // Clear any previous errors
     const queryString = buildQueryString(filters)
+    console.log('Navigating with query string:', queryString)
     navigate(`/results${queryString ? `?${queryString}` : ''}`)
   }
 
